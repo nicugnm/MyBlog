8e6236a7ca7ba3bdf1f05c26cf319a57ea7b260c
nicolaemariusghergu <nghergu17@gmail.com>
2023-01-09 15:34:31 +0200

    Change port to 8080

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const fs = require('fs')
 
 const app = express()
-const port = 3000
+const port = 8080
 
 app.listen(port, console.log(`server started on port ${port}`))
 
